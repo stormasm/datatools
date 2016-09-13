d63e0125e3074d6808b0a40bde380ee1afa9ab7f
Michael Angerman <stormasm@gmail.com>
2016-09-13 11:17:11 -0700

    code cleanup

--- chhs/json02.go
+++ chhs/json02.go
@@ -3,10 +3,10 @@ package main
 import (
 	// "bufio"
 	// "bytes"
-	"io/ioutil"
+	"encoding/json"
 	"fmt"
-    "encoding/json"
-    "reflect"
+	"io/ioutil"
+	"reflect"
 )
 
 func check(e error) {
@@ -16,69 +16,50 @@ func check(e error) {
 }
 
 func processAry(vv []interface{}) {
-    fmt.Println("type of Big Array = ", reflect.TypeOf(vv))
-    fmt.Println("length of Big Array = ", len(vv))
-    for k, u := range vv {
-        fmt.Println("type of Small Array = ", reflect.TypeOf(u))
-        fmt.Println(k, u)
+	fmt.Println("type of Big Array = ", reflect.TypeOf(vv))
+	fmt.Println("length of Big Array = ", len(vv))
+	for k, u := range vv {
+		fmt.Println("type of Small Array = ", reflect.TypeOf(u))
+		fmt.Println(k, u)
 
-
-        switch vv := u.(type) {
-        case string:
-            fmt.Println(k, "is string", vv)
-        case int:
-            fmt.Println(k, "is int", vv)
-        case []interface{}:
-            processAry(vv)
-        default:
-            fmt.Println(k, "is of a type I don't know how to handle")
-        }
-
-
-
-
-    }
+		switch vv := u.(type) {
+		case string:
+			fmt.Println(k, "is string", vv)
+		case int:
+			fmt.Println(k, "is int", vv)
+		case []interface{}:
+			processAry(vv)
+		default:
+			fmt.Println(k, "is of a type I don't know how to handle")
+		}
+	}
 }
 
 func processJson(v interface{}) {
 
-    fmt.Println("type:", reflect.TypeOf(v))
+	fmt.Println("type:", reflect.TypeOf(v))
 
-    m := v.(map[string]interface{})
+	m := v.(map[string]interface{})
 
-    for k, v := range m {
-        switch vv := v.(type) {
-        case string:
-            fmt.Println(k, "is string", vv)
-        case int:
-            fmt.Println(k, "is int", vv)
-        case []interface{}:
-            processAry(vv)
-        default:
-            fmt.Println(k, "is of a type I don't know how to handle")
-        }
-    }
-
-/*
-    switch v := i.(type) {
-    case int:
-        fmt.Println("twice i is", v*2)
-    case float64:
-        fmt.Println("the reciprocal of i is", 1/v)
-    case string:
-        h := len(v) / 2
-        fmt.Println("i swapped by halves is", v[h:]+v[:h])
-    default:
-        fmt.Println("i isn't one of the types above")
-    }
-*/
+	for k, v := range m {
+		switch vv := v.(type) {
+		case string:
+			fmt.Println(k, "is string", vv)
+		case int:
+			fmt.Println(k, "is int", vv)
+		case []interface{}:
+			processAry(vv)
+		default:
+			fmt.Println(k, "is of a type I don't know how to handle")
+		}
+	}
 }
 
 func main() {
-    var v interface{}
+	var v interface{}
 	data, err := ioutil.ReadFile("chem03.json")
 	check(err)
-    err = json.Unmarshal(data, &v)
-    check(err)
-    processJson(v)
+	err = json.Unmarshal(data, &v)
+	check(err)
+	processJson(v)
 }
