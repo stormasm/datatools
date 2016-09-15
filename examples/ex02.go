baba318853a3b067bf9b7c363346898b9d4779c2
Michael Angerman <stormasm@gmail.com>
2016-09-15 08:04:10 -0700

    update ex02

--- examples/ex02.go
+++ examples/ex02.go
@@ -11,16 +11,23 @@ func check(e error) {
 	}
 }
 
-type Document struct {
+type Doc1 struct {
 	Key   string
 	Value interface{}
 }
 
+type Doc2 map[string]interface{}
+
 func main() {
 
-	d := Document{Key: "Name", Value: "Michael"}
+	d1 := Doc1{Key: "Name", Value: "Michael"}
 
-	b, err := json.Marshal(d)
+	b1, err := json.Marshal(d1)
 	check(err)
-	fmt.Println(string(b))
+	fmt.Println(string(b1))
+
+	d2 := make(Doc2)
+	d2["Name"] = "Iris"
+	b2, _ := json.Marshal(d2)
+	fmt.Println(string(b2))
 }
