bec4beb8838876705cadbb6f872cfabbbd8ca0e5
Michael Angerman <stormasm@gmail.com>
2017-04-22 11:16:24 -0700

    cc

--- request/resty01.go
+++ request/resty01.go
@@ -2,24 +2,19 @@ package main
 
 import (
 	"fmt"
-    "github.com/go-resty/resty"
+	"github.com/go-resty/resty"
 )
 
 func main() {
+	resty.SetOutputDirectory("/tmp57")
 
-// Setting output directory path, If directory not exists then resty creates one!
-// This is optional one, if you're planning using absoule path in
-// `Request.SetOutput` and can used together.
-resty.SetOutputDirectory("/tmp57")
+	_, err := resty.R().
+		SetOutput("medreview.json").
+		Get("https://chhs.data.ca.gov/api/views/pch7-48qc/rows.json")
 
-// HTTP response gets saved into file, similar to curl -o flag
-_, err := resty.R().
-          SetOutput("medreview.json").
-          Get("https://chhs.data.ca.gov/api/views/pch7-48qc/rows.json")
+	if err != nil {
+		fmt.Println(err)
+	}
 
-if err != nil {
-    fmt.Println(err)
-}
-
-fmt.Println("done")
+	fmt.Println("done")
 }
